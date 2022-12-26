4ccc21eab24ffcb3c119ec88cb99319ed2a2d1c5
VictoriaSzn <109475339+VictoriaSzn@users.noreply.github.com>
2022-12-26 12:57:59 +0000

    Deploying to gh-pages from @ VictoriaSzn/goit-js-hw-09@8d83653af9f2bb382bf3921cbb03fe09165b74a6 🚀

---
+++ 01-color-switcher.443e44bb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),timerId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.443e44bb.js.map
